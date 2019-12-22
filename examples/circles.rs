1470dad2ff659205ac6402f23f960514999358ed
Ken <kenakioreed@gmail.com>
2019-12-22 11:41:55 -0500

    updated example

--- examples/circles.rs
+++ examples/circles.rs
@@ -18,6 +18,7 @@ fn main() {
 
     let mut timer = very_simple_2d::RefreshTimer::new(16);
 
+    let mut counter=0;
     events_loop.run(move |event, _, control_flow| match event {
         Event::WindowEvent { event, .. } => match event {
             WindowEvent::KeyboardInput { input, .. } => match input.virtual_keycode {
@@ -33,18 +34,34 @@ fn main() {
         },
         Event::EventsCleared => {
             if timer.is_ready() {
+
                 let mut sys = glsys.get_sys();
+
+                sys.arrows([0.0,1.0,0.1,0.5],5.0)
+                .add(vec2(40.,40.),vec2(40.,200.))
+                .add(vec2(40.,40.),vec2(200.,40.))
+                .draw();
+
+
                 {
-                    let mut k = sys.circles([0., 1., 1., 0.1],100.0);
-                    for x in (0..1000).step_by(100) {
-                        for y in (0..1000).step_by(100) {
-                            k.add(vec2(x as f32, y as f32));
+                    let c=counter as f32*0.03;
+                  
+                    let mut k = sys.circles([0., 1., 1., 0.1],4.0);
+                    for x in (0..1000).step_by(12) {
+                        for y in (0..1000).step_by(12) {
+                            let pos=vec2(x,y).inner_as();
+                            k.add(pos+vec2(c.sin()*y as f32*0.1,c.cos()*x as f32*0.1));
                         }
                     }
                     k.draw();
                 }
                 {
-                    let mut k = sys.squares([1., 0., 1., 0.1],100.0);
+                    let c=((counter as f32*0.06).sin()*40.0).abs();
+                    sys.circles([0.5,0.5,1.0,0.6],c).add(vec2(520.,400.)).draw();
+                }
+
+                {
+                    let mut k = sys.squares([1., 0., 1., 0.1],10.0);
                     for x in (0..1000).step_by(100) {
                         for y in (0..1000).step_by(100) {
                             k.add(vec2(x as f32, y as f32));
@@ -56,23 +73,34 @@ fn main() {
                 sys.lines([0., 1.0, 0., 0.3],10.0)
                     .add(vec2(0., 0.), vec2(600., 600.))
                     .add(vec2(0., 600.), vec2(600., 0.))
-                    .add(vec2(700., 500.), vec2(100., 100.))
                     .draw();
 
-                sys.lines([1., 1., 0.2, 0.2],10.)
-                    .add(vec2(50., 500.), vec2(500., 50.))
-                    .draw();
-
-                sys.arrows([0.0,1.0,0.1,0.5],5.0).add(vec2(40.,40.),vec2(40.,200.)).draw();
+                {
+                  let c=counter as f32*0.07;
+                  
+                  sys.lines([1., 1., 0.2, 0.2],10.)
+                      .add(vec2(50., 500.), vec2(500., 50.+c.sin()*50.))
+                      .draw();
+                }
+                
+                {
+                  let c=counter as f32*0.04;
+                  let center=vec2(400.,400.);
+                  sys.arrows([1.0,0.1,0.5,0.5],10.0)
+                  .add(center,center+vec2(c.cos()*80.,c.sin()*80.))
+                  .draw();
+                }
 
                 let mut k = sys.rects([0.8, 0.8, 1.0, 0.2]);
                 k.add(rect(50., 100., 500., 550.));
 
-                k.add(rect(500., 820., 300., 400.));
+                k.add(rect(500., 720., 300., 400.));
                 k.draw();
                 drop(k);
 
                 glsys.swap_buffers();
+
+                counter+=1;
             }
         }
         _ => {}
